11706d20b7d496561bec441a62a74f7ac3be8518
Kevin Laminto <kevin.laminto@gmail.com>
2020-11-05 17:05:51 +1100

    Initial Commit

---
+++ tech-challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tech-challenge
+//
+//  Created by Kevin Laminto on 5/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
