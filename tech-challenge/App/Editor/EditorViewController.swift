9be98480eefddfcc6d2ea39dac477f86c19f0866
Kevin Laminto <kevin.laminto@gmail.com>
2020-11-07 12:21:36 +1100

    Fixed label not aligned properly

--- tech-challenge/App/Editor/EditorViewController.swift
+++ tech-challenge/App/Editor/EditorViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class EditorViewController: UIViewController {
     
+    private let hueSlider: GradientSlider = {
+        let slider = GradientSlider()
+        slider.hasRainbow = true
+        return slider
+    }()
+    private let satSlider: GradientSlider = {
+        let slider = GradientSlider()
+        return slider
+    }()
+    private let brightSlider: GradientSlider = {
+        let slider = GradientSlider()
+        return slider
+    }()
+    private var mStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,8 +38,53 @@ class EditorViewController: UIViewController {
         blurredEffectView.frame = self.view.bounds
         view.addSubview(blurredEffectView)
 
+        // Setup master stackviews
+        let hueSV = createSliderStack(withSlider: hueSlider, withTitle: "Hue")
+        let satSV = createSliderStack(withSlider: satSlider, withTitle: "Saturation")
+        let brightSV = createSliderStack(withSlider: brightSlider, withTitle: "Brightness")
+        
+        mStackView = UIStackView(arrangedSubviews: [hueSV, satSV, brightSV])
+        mStackView.axis = .vertical
+        mStackView.spacing = 20
+        mStackView.distribution = .fillEqually
+        mStackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mStackView)
     }
     
     private func setupConstraint() {
+        mStackView.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+            make.width.equalToSuperview().multipliedBy(0.5)
+            make.height.equalToSuperview().multipliedBy(0.75)
+        }
+    }
+    
+    /// Helper method to create a horizontal stack view with Slider and a title label as its subview.
+    /// - Parameters:
+    ///   - slider: The slider that will be added to the stackview
+    ///   - title: The title of the slider (or label)
+    /// - Returns: StackView from the given parameters
+    private func createSliderStack(withSlider slider: GradientSlider, withTitle title: String) -> UIStackView {
+        // Setup wrapper view so that the label can be left aligned
+        let viewWrapper = UIView()
+        viewWrapper.snp.makeConstraints { (make) in
+            make.width.equalTo(100)
+        }
+        
+        // Setup label
+        let titleLabel = UILabel()
+        titleLabel.text = title
+        titleLabel.textColor = .lightGray
+        
+        viewWrapper.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { (make) in
+            make.left.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        
+        let stackView = UIStackView(arrangedSubviews: [slider, viewWrapper])
+        stackView.spacing = 10
+        
+        return stackView
     }
 }
---
+++ tech-challenge/Extensions/UIStackView.swift
@@ -0,0 +1,17 @@
+//
+//  UIStackView.swift
+//  tech-challenge
+//
+//  Created by Kevin Laminto on 7/11/20.
+//
+
+import UIKit
+
+extension UIStackView {
+    func addBackground(color: UIColor) {
+        let subView = UIView(frame: bounds)
+        subView.backgroundColor = color
+        subView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        insertSubview(subView, at: 0)
+    }
+}
